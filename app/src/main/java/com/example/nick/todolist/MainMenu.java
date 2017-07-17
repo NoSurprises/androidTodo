8e9a774ea53289b57e45f614934287af089f5a97
NoSurprises <helloma@yandex.ru>
2017-07-17 23:58:48 +0300

    Adding a relativelayout view as a new todo

--- app/src/main/java/com/example/nick/todolist/MainMenu.java
+++ app/src/main/java/com/example/nick/todolist/MainMenu.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -15,9 +21,11 @@ public class MainMenu extends AppCompatActivity {
     MenuItem clear;
     MenuItem showAll;
 
-    AddHandler addHandler;
     LinearLayout activities;
     Context context;
+
+    int index = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +33,42 @@ public class MainMenu extends AppCompatActivity {
 
         context = getApplicationContext();
         activities = (LinearLayout) findViewById(R.id.activities);
-        addHandler = new AddHandler(activities, context);
 
-        add = (MenuItem)findViewById(R.id.add);
-        clear = (MenuItem)findViewById(R.id.clear);
-        showAll = (MenuItem)findViewById(R.id.showAll);
 
-        add.setOnMenuItemClickListener(addHandler);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add: {
+
+                View newElem = getLayoutInflater().inflate(R.layout.one_activity, activities);
+                LinearLayout ll
+                        = ((LinearLayout) newElem);
+
+
+                Toast.makeText(context, "Child count " + String.valueOf(ll.getChildCount()), Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.clear: {
+                activities.removeAllViews();
+                break;
+
+            }
+
+            case R.id.showAll: {
+                Toast.makeText(context, "not implemented yet", Toast.LENGTH_SHORT).show();
+                break;
+
+            }
+
+        }
+        return true;
+    }
 }
